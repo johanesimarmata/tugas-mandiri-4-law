5ca2facfd9573a78a585cbf22f2f871373f73a40
Johanes Simarmata <jmpsimarmata@gmail.com>
2022-06-13 22:58:12 +0700

    add exception for npm (if length > 10) and config db

--- service_update/app/api/service.py
+++ service_update/app/api/service.py
@@ -5,6 +5,8 @@ from app.api.db_manager import add_mahasiswa, get_all_mahasiswa
 update_service = APIRouter()
 @update_service.post('/', status_code=201)
 async def update_mahasiswa_service(payload: Mahasiswa):
+    if(len(payload.npm) > 10):
+        raise HTTPException(status_code=404, detail="Max length of NPM Mahasiswa is 10")
     all_mahasiswa = await get_all_mahasiswa()
     for mahasiswa in all_mahasiswa:
         if(mahasiswa.npm == payload.npm):
